78ee6e3417b709e086e7583992ba66029d6b7046
Rômulo Cadó Dorneles <79316323+RomuloCado@users.noreply.github.com>
2021-04-19 09:39:59 -0300

    Create 1116.c

---
+++ 1116.c
@@ -0,0 +1,20 @@
+//Exercício 1116
+
+#include<stdio.h>
+
+int main(){
+	
+	int i, n;
+	float x, y;
+	
+	scanf("%d", &n);
+	for(i=0; i<n; i++){
+		scanf("%f %f", &x, &y);
+		if(y == 0){
+			printf("divisao impossivel\n");
+		}else{
+			printf("%.1f\n", x/y);
+		}
+	}
+	return 0;
+}
