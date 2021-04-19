03dd080e10a707934601ecbe87181da5183b5cee
Rômulo Cadó Dorneles <79316323+RomuloCado@users.noreply.github.com>
2021-04-19 09:59:29 -0300

    Create 1048.c

---
+++ 1048.c
@@ -0,0 +1,37 @@
+// Exercício 1048
+
+#include<stdio.h>
+
+int main(){
+	
+	float salario, reajuste;
+	
+	scanf("%f", &salario);
+	if(salario >=0 && salario <= 400){
+		reajuste = salario * 0.15;
+		printf("Novo salario: %.2f\n", salario + reajuste);
+		printf("Reajuste ganho: %.2f\n", reajuste);
+		printf("Em percentual: 15 %%\n");
+	}else if(salario >=400.01 && salario <= 800){
+		reajuste = salario * 0.12;
+		printf("Novo salario: %.2f\n", salario + reajuste);
+		printf("Reajuste ganho: %.2f\n", reajuste);
+		printf("Em percentual: 12 %%\n");
+	}else if(salario>= 800.01 && salario<= 1200){
+		reajuste = salario * 0.10;
+		printf("Novo salario: %.2f\n", salario + reajuste);
+		printf("Reajuste ganho: %.2f\n", reajuste);
+		printf("Em percentual: 10 %%\n");
+	}else if(salario>= 1200.01 && salario <=2000){
+		reajuste = salario * 0.07;
+		printf("Novo salario: %.2f\n", salario + reajuste);
+		printf("Reajuste ganho: %.2f\n", reajuste);
+		printf("Em percentual: 7 %%\n");
+	}else{
+		reajuste = salario * 0.04;
+		printf("Novo salario: %.2f\n", salario + reajuste);
+		printf("Reajuste ganho: %.2f\n", reajuste);
+		printf("Em percentual: 4 %%\n");
+	}
+	return 0;
+}
