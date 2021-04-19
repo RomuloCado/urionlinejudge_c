2c035857a9e991b08e65e70e3efbb5cc864295ce
Rômulo Cadó Dorneles <79316323+RomuloCado@users.noreply.github.com>
2021-04-19 11:52:25 -0300

    Create 1013.c

---
+++ 1013.c
@@ -0,0 +1,17 @@
+//Exercício 1013
+
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+   int a, b, c, maiorab, maiorc;
+
+   scanf("%d %d %d", &a, &b, &c);
+   
+   maiorab = (a + b + abs(a-b))/2;
+   maiorc = (maiorab + c + abs(maiorab - c))/2;
+   printf("%d eh o maior\n", maiorc);
+   
+   return 0;
+}
